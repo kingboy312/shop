105192f85726b29439768ef440dd2f7a9acbfe24
yqqiu <qyqqiu@163.com>
2020-07-26 17:56:15 +0800

    git add .

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shops',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,10 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'myshop',
+        'USER': 'root',
+        'PASSWORD': 'password'
     }
 }
 
---
+++ shop/shops/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class SuperCat(models.Model):
+    cat_name = models.CharField(max_length=1000)
+    addtime = models.DateTimeField(auto_now_add=True) 
+    def __str__(self):
+        return self.cat_name
+class SubCat(models.Model):
+    cat_name = models.CharField(max_length=1000)
+    addtime = models.DateTimeField(auto_now_add=True)  
+    super_cat_id = models.ForeignKey(SuperCat,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.cat_name
+class Goods(models.Model):
+    name = models.CharField(max_length=255)
+    original_price = models.DecimalField(max_digits=2,max_length=10)
+    current_price  = models.DecimalField(max_digits=2,max_length=10)
+    picture = models.CharField(max_length=255)
+    introduction = models.CharField(max_length=255)
+    views_count = models.AutoField(verbose_name=0)
+    is_sale  = models.BooleanField()
+    is_new = models.BooleanField()
+    supercat_id = models.ForeignKey(SuperCat,on_delete=models.CASCADE)
+    subcat_id = models.ForeignKey(SubCat,on_delete=models.CASCADE)
+    addtime = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
+class Cart(models.Model):
+    subcat_id = models.ForeignKey(Goods,on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    number = models.AutoField(verbose_name=0)
+    addtime = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.number
+class Orders(models.Model):
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    recevie_name = models.CharField(max_length=255)
+    recevie_address = models.CharField(max_length=255)
+    recevie_tel = models.CharField(max_length=255)
+    remark = models.CharField(max_length=255)
+    addtime = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.recevie_name
